e74b0b29a69d576ec36ea561498ff06489cfbd6c
Abdul Baki <abz.baki@gmail.com>
2016-12-30 11:16:04 +0000

    nginx reverse proxy set up for jenkins at install

--- nginx_install.sh
+++ nginx_install.sh
@@ -11,5 +11,8 @@ sudo openssl req -x509 -nodes -days 999 -newkey rsa:2048 \
     -subj "/C=GB/ST=London/L=London/O=localtesting/OU=testing/CN=localhost" \
     -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt
 
+cp /etc/nginx/sites-enabled/default /vagrant/default.orig
+sudo bash -c 'cat /vagrant/jenkins_nginx_rp' > /etc/nginx/sites-enabled/default
+
 sudo service nginx status
 curl localhost
